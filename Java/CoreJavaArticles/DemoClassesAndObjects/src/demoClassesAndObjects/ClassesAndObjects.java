832650c1c533cd6adc2964850fbf9d37247f156f
sshind25 <sandeep_a_shinde@yahoo.co.in>
2019-02-08 07:26:26 +0530

    Added Classes and Objects example for java
    Added Classes and Objects example for java

---
+++ Java/CoreJavaArticles/DemoClassesAndObjects/src/demoClassesAndObjects/ClassesAndObjects.java
@@ -0,0 +1,12 @@
+package demoClassesAndObjects;
+
+public class ClassesAndObjects {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		e1.setId(1);
+		e1.setName("Sam");
+		e1.DisplayInfo();
+	}
+
+}
