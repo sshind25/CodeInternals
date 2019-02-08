bd8b9526e752d10bbf7bbb15644fcbae79a5fb6b
sshind25 <sandeep_a_shinde@yahoo.co.in>
2019-02-08 12:06:09 +0530

    updated

--- Java/CoreJavaArticles/DemoConstructors/src/demoConstructors/ConstructorDemo.java
+++ Java/CoreJavaArticles/DemoConstructors/src/demoConstructors/ConstructorDemo.java
@@ -3,8 +3,13 @@ package demoConstructors;
 public class ConstructorDemo {
 
 	public static void main(String[] args) {
+		//Creating object using parameterized constructor
 		Employee employee = new Employee(1,  "Sam", "Mechanical");
 		employee.DisplayInfo();
+		
+		//Creating object using parameterless constructor
+		Employee employee2 = new Employee();
+		employee2.DisplayInfo();
 	}
 
 }
