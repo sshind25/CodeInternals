d38d6e92f93f4ae4d762a9a741b7d4c35cb98448
sshind25 <sandeep_a_shinde@yahoo.co.in>
2019-02-08 12:08:53 +0530

    updated

--- Java/CoreJavaArticles/DemoConstructors/src/demoConstructors/Department.java
+++ Java/CoreJavaArticles/DemoConstructors/src/demoConstructors/Department.java
@@ -1,9 +1,24 @@
 package demoConstructors;
 
-public class Department {
-	private String deptName;
+public class Employee extends Department {
+	private int id;
+	private String name;
 	
-	public Department(String name) {
-		this.deptName = name;
+	public Employee(){
+		//Constructor chaining. Calling Parameterized constructor in same class
+		this(0, "dummy", "dummyDepartment");
+	}
+	
+	public Employee(int id, String name, String deptName) {
+		//Constructor chaining. Calling base class constructor
+		super(deptName);
+		this.id = id;
+		this.name = name;
+	}
+	
+	public void DisplayInfo() {
+		System.out.println("Employee id: " + id);
+		System.out.println("Employee name: " + name);
+		System.out.println("Department name: " + getDeptName());		
 	}
 }
