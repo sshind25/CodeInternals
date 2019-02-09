43f30c37ea1ec80ce4aebfbc8dd2c0ed45ccb486
sshind25 <sandeep_a_shinde@yahoo.co.in>
2019-02-09 19:49:06 +0530

    updated

--- Java/CoreJavaArticles/DemoPolymorphism/src/runtimepolymorphism/DemoRuntime.java
+++ Java/CoreJavaArticles/DemoPolymorphism/src/runtimepolymorphism/DemoRuntime.java
@@ -4,16 +4,16 @@ public class DemoRuntime {
 
 	public static void main(String[] args) {
 		//class1 holds object of Class1
-		Class1 class1 = new Class1();
-		class1.displayMessage();
+		Class1 class11 = new Class1();
+		class11.displayMessage();
 		
 		//class2 holds object of Class2
-		Class1 class2 = new Class2();
-		class2.displayMessage();
+		Class1 class22 = new Class2();
+		class22.displayMessage();
 		
 		//class22 holds object of Class2
-		Class2 class22 = new Class2();
-		class22.displayMessage();
+		Class2 class33 = new Class2();
+		class33.displayMessage();
 	}
 
 }
